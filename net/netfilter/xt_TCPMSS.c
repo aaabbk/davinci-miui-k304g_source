4577788c0fbfdf70e66e4d69880a4bbec8ae2f18
aaabbk <2268951925@qq.com>
2025-08-18 22:24:41 +0800

    fix error

--- net/netfilter/xt_TCPMSS.c
+++ net/netfilter/xt_TCPMSS.c
@@ -78,6 +78,19 @@ dropit:
 	return false;
 }
 
+/* Must specify -p tcp --syn */
+static inline bool find_syn_match(const struct xt_entry_match *m)
+{
+	const struct xt_tcp *tcpinfo = (const struct xt_tcp *)m->data;
+
+	if (strcmp(m->u.kernel.match->name, "tcp") == 0 &&
+	    tcpinfo->flg_cmp & TCPHDR_SYN &&
+	    !(tcpinfo->invflags & XT_TCP_INV_FLAGS))
+		return true;
+
+	return false;
+}
+
 static int tcpmss_tg4_check(const struct xt_tgchk_param *par)
 {
 	const struct xt_tcpmss_info *info = par->targinfo;
